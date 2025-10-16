8c509aa8530bdc2a95a18769b935fa76e6f11d86
lova <lova.anand@gmail.com>
2025-10-16 20:38:29 +0530

    First Commit

---
+++ src/test/java/com/lova/DockerJenkinIntegrationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinIntegrationSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
