5800a26ee8cc5d677e7cb8b1f1fb8b1f1c2d330c
lova <lova.anand@gmail.com>
2025-10-16 21:57:22 +0530

    second Commit

--- src/main/java/com/lova/DockerJenkinIntegrationSampleApplication.java
+++ src/main/java/com/lova/DockerJenkinIntegrationSampleApplication.java
@@ -1,13 +1,22 @@
 package com.lova;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DockerJenkinIntegrationSampleApplication {
+public class DockerJenkinIntegrationSampleApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinIntegrationSampleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println(
+				"Hello World! This is a sample Spring Boot application running in Docker and integrated with Jenkins.");
+
 	}
 
 }
